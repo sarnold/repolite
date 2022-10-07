1023f3b1a155facb6a7509347b00f17cb2689053
Stephen L Arnold <nerdboy@gentoo.org>
2022-10-07 13:16:04 -0700

    fix: dev: make setup.py compatible with older python, eg, py36 bionic
    * required for python envs still using distutils that do not
      like projects with src/ layout
    * update tox file to generate egg_info for mypy
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
-import setuptools
+import sys
 
-if __name__ == "__main__":
+if sys.version_info > (3,6):
+    import setuptools
+
     setuptools.setup()
+else:
+    from distutils.core import setup
+
+    pkg_name = 'repolite'
+    sys.path[0:0] = ['src'/pkg_name]
+    from _version import __version__
+
+    setup(
+        name=pkg_name,
+        version=__version__,
+        packages=[pkg_name],
+        package_dir = {'': 'src'}
+    )
