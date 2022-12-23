0edaac7c140fc59b2244454bdb07d0243007f9e9
Stephen L Arnold <nerdboy@gentoo.org>
2022-12-22 19:13:27 -0800

    new: usr: add new config options, update readme and default yaml
    * add option for clone depth with default 0 (ala gh workflows)
    * add option to install a repo into the current env using pip
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- src/repolite/__init__.py
+++ src/repolite/__init__.py
@@ -1,9 +1,9 @@
 """
-Repolite clones repos according to your local yaml config file.
+Repolite manages git repos according to your local yaml config file.
 """
 
 from ._version import __version__
 
-__description__ = "Lite repository manager for small-ish projects."
+__description__ = "Lite git repository manager for small-ish projects."
 
 __all__ = ["__version__", "__description__"]
